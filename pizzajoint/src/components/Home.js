7908aef75d3f5bb3cdc14eb25ca94a81371dae58
kharioki <antolisah@gmail.com>
2020-07-17 23:57:39 +0300

    added framer motion and first animate

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
     <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+      <motion.h2 animate={{ fontSize: 50 }}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+        <button>Create Your Pizza</button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
