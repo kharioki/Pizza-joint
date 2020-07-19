5623b096ca90c56a86479ab7d055415a8c19206b
kharioki <antolisah@gmail.com>
2020-07-20 01:55:15 +0300

    modal animation

--- pizzajoint/src/App.js
+++ pizzajoint/src/App.js
@@ -7,10 +7,12 @@ import Home from './components/Home';
 import Base from './components/Base';
 import Toppings from './components/Toppings';
 import Order from './components/Order';
+import Modal from './components/Modal';
 
 function App() {
   const location = useLocation();
   const [pizza, setPizza] = useState({ base: '', toppings: [] });
+  const [showModal, setShowModal] = useState(false);
 
   const addBase = base => {
     setPizza({ ...pizza, base });
@@ -29,6 +31,7 @@ function App() {
   return (
     <>
       <Header />
+      <Modal showModal={showModal} setShowModal={setShowModal} />
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.key}>
           <Route path="/base">
@@ -38,7 +41,7 @@ function App() {
             <Toppings addTopping={addTopping} pizza={pizza} />
           </Route>
           <Route path="/order">
-            <Order pizza={pizza} />
+            <Order pizza={pizza} setShowModal={setShowModal} />
           </Route>
           <Route path="/">
             <Home />
---
+++ pizzajoint/src/components/Modal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const backdropVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdropVariants}
+          initial="hidden"
+          animate="visible"
+        ></motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
